a355a94ab020b10c621ed0fe88a2fbe82c2dfdbb
Oleg Albekov <oleg@albekov.ru>
2023-10-05 20:19:46 +0400

    test: enhance unit testing for progress setting functions

--- progress_test.go
+++ progress_test.go
@@ -27,3 +27,27 @@ func TestFormatBar(t *testing.T) {
 		t.Errorf("formatBar() = %v", progress.renderBar())
 	}
 }
+
+func TestWithRefreshInterval(t *testing.T) {
+	progress := New(WithRefreshInterval(500 * time.Millisecond))
+	if progress.RefreshInterval != 500*time.Millisecond {
+		t.Errorf("WithRefreshInterval() = %v", progress.RefreshInterval)
+	}
+}
+
+func TestSet(t *testing.T) {
+	progress := New()
+	progress.Set(25)
+	if progress.current != 25 {
+		t.Errorf("Set() = %v", progress.current)
+	}
+}
+
+func TestInc(t *testing.T) {
+	progress := New()
+	progress.Inc()
+	progress.Inc()
+	if progress.current != 2 {
+		t.Errorf("Inc() = %v", progress.current)
+	}
+}
